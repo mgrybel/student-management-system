9ae744223e87ee79ce594816f73796bddc79ca68
mgrybel <grybel.m.m@gmail.com>
2024-03-03 23:46:10 +0100

    Added tests

---
+++ backend/src/test/java/com/students/StudentRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.students;
+
+import com.students.domain.Student;
+import com.students.domain.StudentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository repository;
+
+    @Test
+    void saveStudent() {
+        repository.save(new Student("Bob", "Smith", "bob@test.com", "Computer Science", 412341, 4));
+        assertThat(repository.findByFieldOfStudy("Computer Science").isPresent()).isTrue();
+    }
+
+    @Test
+    void deleteStudent() {
+        repository.save(new Student("John", "Doe", "john@test.com", "Physics", 323341, 4));
+        repository.deleteAll();
+        assertThat(repository.count()).isEqualTo(0);
+    }
+}
